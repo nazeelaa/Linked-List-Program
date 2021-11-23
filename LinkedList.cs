bb414e6d86a512629d4ec3e8c246f34acb7fbbe3
nazeelaa <nazeelakudari949@gmail.com>
2021-11-23 12:48:29 +0530

    pushed

---
+++ LinkedList.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day14PracticeProblems
+{
+    class LinkedList
+    {
+        internal Node head;
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = (Node)temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into linked list", node.data);
+        }
+        internal void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("LinkedList is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = (Node)temp.next;
+            }
+        }
+
+      
+        }
+    }
+
+
+    
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Day14PracticeProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Linked List Program.");
+            LinkedList List = new LinkedList();
+            List.Add(70);
+            List.Add(30);
+            List.Add(56);
+            List.Display();
+        }
+    }
+}
